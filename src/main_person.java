2f8eabefafda5cb7ba324f85df784bc86c7b46af
belyaeva-zh <e.beliaeva6@alumni.nsu.ru>
2022-09-01 20:06:19 +0700

    Add files for OOP task

---
+++ src/main_person.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class main_person {
+    public static void main(String[] args) {
+        System.out.println("Введите фамилию");
+        Scanner scanner = new Scanner(System.in);
+        String surname = scanner.nextLine();
+
+        System.out.println("Введите имя ");
+        String name = scanner.nextLine();
+
+        System.out.println("Введите отчество ");
+        String middleName = scanner.nextLine();
+
+        System.out.println("Введите возраст ");
+        int age = scanner.nextInt();
+
+        person human = new person(surname, name, middleName, age);
+
+        System.out.println("Возраст созданного объекта: " + human.getAge());
+
+    }
+}
---
+++ src/person.java
@@ -0,0 +1,58 @@
+public class person {
+    private String name;
+    private String middleName;
+    private String surname;
+    private int age;
+
+    public person(String name, String middleName, String surname, int age) {
+        this.name = name;
+        this.middleName = middleName;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "{" + surname + " " + name + " " + middleName + ", " + age + "}";
+    }
+
+
+}
+
+
+
+
+
+
