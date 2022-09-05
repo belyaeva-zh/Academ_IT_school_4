23f92ad71447ff9a6b2699fe3f704ecfae8b0c47
belyaeva-zh <e.beliaeva6@alumni.nsu.ru>
2022-09-05 22:21:34 +0700

    Rename file + edits

---
+++ src/loopFor.java
@@ -0,0 +1,11 @@
+public class loopFor {
+    public static boolean isDivisibleByFour(int x) {
+        return x % 4 == 0;
+    }
+
+    public static void main(String[] args) {
+        for (int i = 100; i >= 1; i--) {
+            if (isDivisibleByFour(i)) System.out.print(i + " ");
+        }
+    }
+}
